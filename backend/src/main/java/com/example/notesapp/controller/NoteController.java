1bbac1ef7a6b04f611fa48456df63d83e309ac95
22KQ1A05I1 <pavanpavanobn@gmail.com>
2026-05-02 09:40:52 +0530

    Update NoteController.java

--- backend/src/main/java/com/example/notesapp/controller/NoteController.java
+++ backend/src/main/java/com/example/notesapp/controller/NoteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/notes")
+@RequestMapping("/")
 @CrossOrigin("*")
 public class NoteController {
 
